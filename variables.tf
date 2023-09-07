3b096919c7b5cd5e634e4ec6e9eade7fc3b252d1
Gerric Chaplin <1778675+gerricchaplin@users.noreply.github.com>
2023-09-07 20:50:59 +0100

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "cur_bucket_name" {
+  type        = string
+  description = "The name of the bucket which will be used to store the CUR date for Vertice."
+}
+
+variable "vertice_account_ids" {
+  type        = list(string)
+  description = "List of Account IDs, which are allowed to access the role."
+  default     = ["642184526628", "762729743961"]
+}
