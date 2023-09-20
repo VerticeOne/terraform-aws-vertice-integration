33ba72bb9ecf276a9dbdc40c318fa770fd25ef6c
Gerric Chaplin <1778675+gerricchaplin@users.noreply.github.com>
2023-09-20 18:16:21 +0100

    Minor updates to documentation and var descriptions

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "cur_bucket_name" {
   type        = string
-  description = "The name of the bucket which will be used to store the CUR date for Vertice."
+  description = "The name of the bucket which will be used to store the CUR data for Vertice."
 }
 
 variable "vertice_account_ids" {
   type        = list(string)
-  description = "List of Account IDs, which are allowed to access the role."
+  description = "List of Account IDs, which are allowed to access the Vertice cross account role."
   default     = ["642184526628", "762729743961"]
 }
