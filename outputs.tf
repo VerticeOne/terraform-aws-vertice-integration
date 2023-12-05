44b15f154cf389144428013ce6b56a8226ac418a
Kristián Leško <kristian.lesko@vertice.one>
2023-12-05 10:15:42 +0100

    FEATURE CE-365 Export opinionated default as outputs (#11)

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,17 @@
+output "cur_report_name" {
+  description = "Name of the CUR report created."
+  value       = var.cur_report_enabled ? var.cur_report_name : null
+}
+
+output "cur_report_s3_prefix" {
+  description = "Name of the S3 prefix used by the CUR report."
+  value       = var.cur_report_enabled ? var.cur_report_s3_prefix : null
+}
+
+output "vertice_account_ids" {
+  description = "Account IDs of Vertice allowed to access your AWS resources."
+  value       = var.governance_role_enabled ? var.vertice_account_ids : null
+}
 
 output "vertice_governance_role_arn" {
   description = "The ARN of VerticeGovernance role created."
