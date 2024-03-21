96e305938e25d6c4c993146d968b0ea39cec690c
Kristian Lesko <kristian.lesko@vertice.one>
2024-03-21 12:49:46 +0100

    FEATURE CE-524: Enable role assume policy override

--- variables.tf
+++ variables.tf
@@ -40,6 +40,12 @@ variable "governance_role_external_id" {
   default     = ""
 }
 
+variable "governance_role_assume_policy_json" {
+  type        = string
+  description = "Optional override for VerticeGovernanceRole assume policy. Default assume role policy is constructed if this is not provided."
+  default     = null
+}
+
 variable "governance_role_additional_policy_json" {
   type        = string
   description = "Custom additional policy in JSON format to attach to VerticeGovernance role. Default is null for no additional policy."
