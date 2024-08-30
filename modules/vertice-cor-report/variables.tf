1a3325918d4920263586a885b54d2e4eee3baed2
Miroslav Zapletal <miroslav.zapletal@vertice.one>
2024-08-30 15:17:44 +0200

    FEATURE ASH-32: Changed wildcard to specific columns

--- modules/vertice-cor-report/variables.tf
+++ modules/vertice-cor-report/variables.tf
@@ -16,7 +16,13 @@ variable "cor_report_s3_prefix" {
 }
 
 variable "cor_columns_for_selection" {
-  default     = []
+  default = ["account_id", "action_type", "currency_code", "current_resource_details", "current_resource_summary",
+    "current_resource_type", "estimated_monthly_cost_after_discount", "estimated_monthly_cost_before_discount",
+    "estimated_monthly_savings_after_discount", "estimated_monthly_savings_before_discount",
+    "estimated_savings_percentage_after_discount", "estimated_savings_percentage_before_discount",
+    "implementation_effort", "last_refresh_timestamp", "recommendation_id", "recommendation_lookback_period_in_days",
+    "recommendation_source", "recommended_resource_details", "recommended_resource_summary", "recommended_resource_type",
+  "region", "resource_arn", "restart_needed", "rollback_possible", "tags"]
   description = "The list of names of columns that you want to select from COST_OPTIMIZATION_RECOMMENDATIONS table"
   type        = list(string)
 }
