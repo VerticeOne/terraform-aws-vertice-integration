ce849c039cc27661657602a8a523f387c1e396ca
Miroslav Zapletal <miroslav.zapletal@vertice.one>
2024-08-30 10:50:05 +0200

    FEATURE ASH-32: Fixed issue with wrongly declared variable

--- modules/vertice-cor-report/main.tf
+++ modules/vertice-cor-report/main.tf
@@ -17,9 +17,9 @@ resource "aws_bcmdataexports_export" "vertice_cor_report" {
     }
     destination_configurations {
       s3_destination {
-        s3_bucket = aws_s3_bucket.vertice_cor_bucket.bucket
+        s3_bucket = data.aws_s3_bucket.vertice_cor_bucket.bucket
         s3_prefix = var.cor_report_s3_prefix
-        s3_region = aws_s3_bucket.vertice_cor_bucket.region
+        s3_region = data.aws_s3_bucket.vertice_cor_bucket.region
         s3_output_configurations {
           overwrite   = "OVERWRITE_REPORT"
           format      = "PARQUET"
